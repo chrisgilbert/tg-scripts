cd4cf37e033b22920f6b397115c40fe47c597a90
Aaron Griffin <griffinaaronj@gmail.com>
2022-03-11 13:54:26 -0700

    correct one-liner command and test use of one line

--- Client/Windows/preferIPv4.ps1
+++ Client/Windows/preferIPv4.ps1
@@ -2,7 +2,7 @@
 #Applies registry key to prefer IPv4 over IPv6: https://docs.microsoft.com/en-US/troubleshoot/windows-server/networking/configure-ipv6-in-windows#use-registry-key-to-configure-ipv6
 #
 #Can be ran as a one-liner with the below command in powershell ran as administrator:
-#preferIPv4 = Invoke-WebRequest https://raw.githubusercontent.com/Twingate-Labs/tg-scripts/main/Client/Windows/preferIPv4.ps1 && Invoke-Expression $($preferIPv4.Content)
+#preferIPv4 = Invoke-WebRequest https://raw.githubusercontent.com/Twingate-Labs/tg-scripts/main/Client/Windows/preferIPv4.ps1; Invoke-Expression $($preferIPv4.Content)
 
 function Set-PreferIPv4 () {
     try {
@@ -22,10 +22,10 @@ function Set-PreferIPv4 () {
         }
         catch {
             if ($($PSItem.ToString()) -eq "Requested registry access is not allowed.") {
-                Write-Output "Error: $($PSItem.ToString()) Powershell must be ran as administrator to run this script." -ForegroundColor Red
+                Write-Output "Error:" $($PSItem.ToString()) "Powershell must be ran as administrator to run this script." -ForegroundColor Red
               } 
               else {
-                Write-Output "Error: $($PSItem.ToString())" -ForegroundColor Red
+                Write-Output "Error:" $($PSItem.ToString()) -ForegroundColor Red
               }
         }
       }
